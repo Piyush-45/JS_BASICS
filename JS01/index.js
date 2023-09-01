efaf303dcfa9e595f725e4f737664cd82cb7fd76
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-09-01 13:23:59 +0530

    lecture 1

---
+++ JS01/index.js
@@ -0,0 +1,43 @@
+// ! Variables
+// ? In JavaScript, variables act as containers for storing data.
+
+//   variable in detial; 
+
+// step:1 Declaration   keyword + variable name 
+
+// => let, var, const
+
+var age; // ?Declaration using var
+
+// step: 2. Assignment: 
+age = 25; // ?number 
+
+
+//? INITIALIZATION
+let name = "123479";  // declaration + assignment === initialization
+console.log(name)// string
+
+let bool = false; // ?boolean
+console.log(bool)
+
+let middleName = null // ?null
+console.log(middleName)
+
+let jobtitle;
+
+
+// ?👉 "null" is a value that represents intentional absence or lack of value. It is often used to indicate that a variable or property should have no value.
+
+// ? 👉 "undefined" usually indicates that something hasn't been initialized or doesn't exist.
+
+
+// ! ways of declaring variables in js
+
+function check (){
+    let bool = true
+    console.log(bool)
+}
+check()
+
+
+
